b4895986addc9be340fb4efe115eabea685b27e4
Bruno <bruno.barbosa8@yahoo.com>
2025-01-10 16:22:20 -0500

    Finished class content

--- W1-AI_InClassExercise/Assets/ParadoxNotion/Scripts/DebugAT.cs
+++ W1-AI_InClassExercise/Assets/ParadoxNotion/Scripts/DebugAT.cs
@@ -1,5 +1,6 @@
 using NodeCanvas.Framework;
 using ParadoxNotion.Design;
+using UnityEngine;
 
 
 namespace NodeCanvas.Tasks.Actions {
@@ -13,6 +14,7 @@ namespace NodeCanvas.Tasks.Actions {
 			//getComponent
 			//get the initial possition of character
 			//FindObject
+			Debug.Log("initializaing the state");
 			return null;
 		}
 
@@ -20,12 +22,24 @@ namespace NodeCanvas.Tasks.Actions {
 		//Call EndAction() to mark the action as finished, either in success or failure.
 		//EndAction can be called from anywhere.
 		protected override void OnExecute() {
-			EndAction(true);
+            //seting a booelan at the start
+            //flipping a character upside down
+            //setting dynamic varaibles tat can change over time
+            //reseting values when uou jump into the state
+
+
+            Debug.Log("executing the state");
+
+
+            EndAction(true);
 		}
 
 		//Called once per frame while the action is active.
 		protected override void OnUpdate() {
-			
+			Debug.Log("update is running");
+			//agent.transform.position += agent.transform.forward * 3f * Time.deltaTime;
+			//Rigidbody rb = agent.GetComponent<Rigidbody>();
+			//agent.gameObject.SetActive(false);
 		}
 
 		//Called when the task is disabled.
