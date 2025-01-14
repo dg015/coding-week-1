2174fefbcf2cd95a167bfe91283b9e3db691274c
dg015 <bruno.barbosa8@yahoo.com>
2025-01-14 16:58:49 -0500

    Door openeing code working

--- W1-AI_InClassExercise/Assets/ParadoxNotion/Scripts/Assigment 1 scripts/DoorToggle.cs
+++ W1-AI_InClassExercise/Assets/ParadoxNotion/Scripts/Assigment 1 scripts/DoorToggle.cs
@@ -4,30 +4,48 @@ using UnityEngine;
 
 public class DoorToggle : MonoBehaviour
 {
-    public bool doorToggle;
+    public bool doorToggle = true;
     public bool doorStatus = false;
-    private GameObject door;
+
+    //false = door closed 
+    //true = door open
+    public GameObject door;
     // Start is called before the first frame update
     void Start()
     {
-        door = GameObject.Find("Door").GetComponent<GameObject>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (doorStatus == false && doorToggle == true)
-        {
+        ToggleDoor();
+    }
 
-        }
-        else if (doorStatus == true && doorToggle == true)
+    private void ToggleDoor()
+    {
+        if(Input.GetKeyDown(KeyCode.C))
         {
+            Vector2 DoorPosition = new Vector2(0, 0);
+            if (doorStatus == false && doorToggle == true)
+            {
+                DoorPosition = new Vector2(5.75f, 3.14f);
+                door.transform.transform.position = DoorPosition;
+                doorStatus = true;
+            }
+            else if (doorStatus == true && doorToggle == true)
+            {
+                DoorPosition = new Vector2(5.75f, -0.4858583f);
+                door.transform.transform.position = DoorPosition;
+                doorStatus = false;
+            }
+            else
+            {
+                Debug.LogError("door not ready for toggle function");
 
-        }
-        else
-        {
-            Debug.LogError("door not ready for toggle function");
+            }
 
         }
     }
+
 }
