6ef5113261c94f968b8e11da26d9696ec0a25f2b
dg015 <bruno.barbosa8@yahoo.com>
2025-01-14 16:43:35 -0500

    created and added basic script for cheking if door is open and if toggle is ready or not

--- W1-AI_InClassExercise/Assets/ParadoxNotion/Scripts/Assigment 1 scripts/DoorToggle.cs
+++ W1-AI_InClassExercise/Assets/ParadoxNotion/Scripts/Assigment 1 scripts/DoorToggle.cs
@@ -6,15 +6,28 @@ public class DoorToggle : MonoBehaviour
 {
     public bool doorToggle;
     public bool doorStatus = false;
+    private GameObject door;
     // Start is called before the first frame update
     void Start()
     {
-        
+        door = GameObject.Find("Door").GetComponent<GameObject>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (doorStatus == false && doorToggle == true)
+        {
+
+        }
+        else if (doorStatus == true && doorToggle == true)
+        {
+
+        }
+        else
+        {
+            Debug.LogError("door not ready for toggle function");
+
+        }
     }
 }
